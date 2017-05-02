d9b31b186c15664384e9f78e358b6f28b334e182
Chris Washer <cwasher@users.noreply.github.com>
2017-05-02 07:45:35 -0500

    Update Set-DominateUser.ps1

--- Set-DominateUser.ps1
+++ Set-DominateUser.ps1
@@ -1,49 +1,61 @@
-##################################################
-# Determines the 'Dominate User' of the computer #
-##################################################
-$UserArray = @()
-Get-ChildItem -Path (Get-Item -Path $env:USERPROFILE).PSParentPath -Exclude Default*, Public | 
-Select-Object -Property Name, CreationTimeUtc, LastAccessTimeUtc |
-            ForEach-Object {
-            $NewObj = New-Object -TypeName PSObject
-            $TimeInterval = New-TimeSpan -Start $_.CreationTimeUtc -End $_.LastAccessTimeUtc 
-            $NewObj | Add-Member -Type NoteProperty -Name User -Value $_.Name
-            $NewObj | Add-Member -Type NoteProperty -Name Interval -Value $TimeInterval
-            $UserArray += $NewObj
-            }
-$DominateUser = ($UserArray | Sort Interval -Descending | Select-Object -First 1).User
+<#
+.SYNOPSIS
+Determines the Dominate User on the computer and writes that value to the ManagedBy property in Active Directory.
 
-##############################################
-# Gets the user object from Active Directory #
-##############################################
+.DESCRIPTION
+The Dominate User is determined by taking the greatest difference between the LastAccessTimeUTC and the CreationTimeUTC. The user and computer objects are pulled from Active Directory by using ADSI. 
+#>
+[CmdletBinding()]
+Param()
 
-$strName = $DominateUser
+function Get-MBDominateUser {
+    [CmdletBinding()]
+    Param()
 
-$strFilter = "(&(objectCategory=User)(samAccountName=$strName))"
-$objSearcher = New-Object System.DirectoryServices.DirectorySearcher 
-$objSearcher.Filter = $strFilter
+    Write-Verbose "Get array of local users. Determine Dominate User based on largest TimeInterval."
+    $UserArray = @()
+    Get-ChildItem -Path (Get-Item -Path $env:USERPROFILE).PSParentPath -Exclude Default*, Public | 
+    Select-Object -Property Name, CreationTimeUtc, LastAccessTimeUtc |
+                ForEach-Object {
+                $NewObj = New-Object -TypeName PSObject
+                $TimeInterval = New-TimeSpan -Start $_.CreationTimeUtc -End $_.LastAccessTimeUtc 
+                $NewObj | Add-Member -Type NoteProperty -Name User -Value $_.Name
+                $NewObj | Add-Member -Type NoteProperty -Name Interval -Value $TimeInterval
+                $UserArray += $NewObj
+                }
+    $DominateUser = ($UserArray | Sort Interval -Descending | Select-Object -First 1).User
 
-$objPath = $objSearcher.FindOne() 
-$objUser = $objPath.GetDirectoryEntry()
+    Write-Verbose "Use ADSI to determine user DN"
+    $strName = $DominateUser
 
-$strLoggedInUser = $objUser.DistinguishedName
+    $strFilter = "(&(objectCategory=User)(samAccountName=$strName))"
+    $objSearcher = New-Object System.DirectoryServices.DirectorySearcher 
+    $objSearcher.Filter = $strFilter
 
-#################################################
-# Get the computer object from Active Directory #
-#################################################
+    $objPath = $objSearcher.FindOne() 
+    $objUser = $objPath.GetDirectoryEntry()
 
-$strComputer = $env:computername
+    Write-Output $objUser
+}
 
-$strFilter = "(&(objectCategory=Computer)(cn=$strComputer))"
-$objSearcher = New-Object System.DirectoryServices.DirectorySearcher 
-$objSearcher.Filter = $strFilter
+function Get-MBComputer {
+    [CmdletBinding()]
+    Param()
 
-$objPath = $objSearcher.FindOne() 
-$objComputer = $objPath.GetDirectoryEntry()
+    Write-Verbose "Use ADSI to determine computer DN"
+    $strComputer = $env:computername
 
-###################################################
-# Set ManagedBy attribute of the computer object. #
-###################################################
+    $strFilter = "(&(objectCategory=Computer)(cn=$strComputer))"
+    $objSearcher = New-Object System.DirectoryServices.DirectorySearcher 
+    $objSearcher.Filter = $strFilter
 
-$objComputer.ManagedBy = $strLoggedInUser
-$objComputer.CommitChanges()
+    $objPath = $objSearcher.FindOne() 
+    $objComputer = $objPath.GetDirectoryEntry()
+    Write-Output $objComputer
+}
+
+Write-Verbose "Writing Dominate User value to Active Directory"
+$DominateUserDN = (Get-MBDominateUser).distinguishedName
+$MBComputer = Get-MBComputer
+$MBComputer.managedBy = $DominateUserDN
+$MBComputer.CommitChanges()
